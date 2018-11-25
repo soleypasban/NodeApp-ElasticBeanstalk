21780ed6dd5b8ed93b753c446687c568b1396bde
Soley Pasban <pasban110@gmail.com>
2018-11-25 21:16:53 +0200

    added sync file reading

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var http = require('http'),
-    fs = require('fs');
+var port = process.env.PORT || 80,
+    http = require('http'),
+    fs = require('fs')
 
-fs.readFile('./content.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(80);
-});
+var html = fs.readFileSync('content.html')
+
+var server = http.createServer(function (request, response) {
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    response.write(html);
+    response.end();
+})
+
+server.listen(port);
+
+console.log('Server running at http://localhost:' + port + '/');
